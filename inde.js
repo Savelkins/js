b32422ae1ceb5ebb462948b836763859ee92f2c0
Savelkins <savakirilchuk@gmail.com>
2024-06-17 19:03:09 +0300

    js-function-practice

---
+++ inde.js
@@ -0,0 +1,79 @@
+// /**
+//  *
+//  * @param {number} year
+//  * @returns {boolean}
+//  */
+// function calculateMultiple100Years (year = 2024){
+//   return (year % 100 === 0)
+// }
+
+// console.log(calculateMultiple100Years(2000))
+
+// const ZERO_FAHRENHEIT = 32;
+// const FOEF_CELSIUS_FAHRENHEIT = 9/5;
+// /**
+//  *
+//  * @param {number} celsius
+//  * @returns {number}
+//  */
+// function convertCelsiusFahrenheit(celsius){
+//   return (celsius * FOEF_CELSIUS_FAHRENHEIT + ZERO_FAHRENHEIT);
+// }
+// console.log(convertCelsiusFahrenheit(0))
+
+// /**
+//  *
+//  * @param {number} num1
+//  * @param {number} num2
+//  * @returns {boolean}
+//  */
+// function calculatorMultiplesNumbers(num1, num2) {
+//   return (num1%num2 === 0);
+// }
+// console.log(calculatorMultiplesNumbers(1, 10))
+
+// /**
+//  *
+//  * @param {number} basis
+//  * @param {number} height
+//  * @returns {number}
+//  */
+// function calculateAreaTrilage(basis, height) {
+//   return (basis * height) / 2;
+// }
+// console.log(calculateAreaTrilage(10, 15))
+
+// /**
+//  *
+//  * @param {number} price
+//  * @param {number} tax default = 20
+//  * @returns {number}
+//  */
+// function priceProductIncludingTax(price, tax = 20) {
+//   const procentCalculate = (price * tax) / 100;
+//   return price + procentCalculate;
+// }
+// console.log(priceProductIncludingTax(10))
+
+// /**
+//  *
+//  * @param {number} num1
+//  * @param {number} num2
+//  * @returns {boolean}
+//  */
+// function isDifferenceBetweenNumber (num1, num2){
+//   return (num1 - num2)%2 === 0
+// }
+// console.log(isDifferenceBetweenNumber(20, 8))
+
+// const INCHES_CENTIMETERS = 2.54;
+
+// /**
+//  *
+//  * @param {number} Inches
+//  * @returns {number}
+//  */
+// function convertInchesCentimeters(inches) {
+//   return inches * INCHES_CENTIMETERS;
+// }
+// console.log(convertInchesCentimeters(10));
